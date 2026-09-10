07847ca7584b134e6e54bd8d6b08f5d8edd8f6cb
Yuki Little <nevan.little@hotmail.com>
2026-09-10 11:21:36 -0600

    fixed merge conflicts

--- 01_Scripts/Puzzle/pressure_plate.gd
+++ 01_Scripts/Puzzle/pressure_plate.gd
@@ -4,18 +4,14 @@ var ElementsOnPlate: int = 0
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_method("triggers_pressure_plates"):
-		ElementsOnPlate += 1
-		if ElementsOnPlate == 1:
-			ActivatePuzzle()
+	ElementsOnPlate += 1
+	if ElementsOnPlate == 1:
+		ActivatePuzzle()
 	
-	pass # Replace with function body.
 
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.has_method("triggers_pressure_plates"):
-		ElementsOnPlate -= 1
-		if ElementsOnPlate == 0:
-			ActivatePuzzle()
-	pass # Replace with function body.
+	ElementsOnPlate -= 1
+	if ElementsOnPlate == 0:
+		ActivatePuzzle()
