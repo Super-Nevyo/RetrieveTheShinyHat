e9ca1d32ab5f244c876969cd156021dbaa61670b
miss-nibiru <du.herbst@gmail.com>
2026-09-16 13:11:58 -0600

    Debugging

--- 01_Scripts/_Manager/RoomManager.gd
+++ 01_Scripts/_Manager/RoomManager.gd
@@ -10,13 +10,22 @@ func _ready() -> void:
 	load_room(starting_room)
 
 func load_room(room_scene: PackedScene) -> void:
+	if room_scene == null:
+		print("Assign a room scene in the inspector")
+	
 	if is_instance_valid(current_room):
 		current_room.queue_free()
 	
 	current_room = room_scene.instantiate()
 	room_container.add_child.call_deferred(current_room) #ask room container to add the room to a child
 	
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
+	handle_test_input(event)
+
+func handle_test_input(event: InputEvent) -> void:
 	if event.is_action_pressed("test_transition") and not event.is_echo():
 		print("Called room change test")
 		load_room(test_room)
+		
+		
+		#map info gets located InventoryIndo script
