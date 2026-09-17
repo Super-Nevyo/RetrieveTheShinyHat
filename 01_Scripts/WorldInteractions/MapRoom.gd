ac347a0dd4a2993cd4dd65c681322307a63486f3
miss-nibiru <du.herbst@gmail.com>
2026-09-16 19:58:43 -0600

    Added map transition signal
    IDs are manual for now, researching how to make map metadata to hold all exit ids

---
+++ 01_Scripts/WorldInteractions/MapRoom.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name MapRoom
+
+@export var room_id: String
+@export var room_scene: PackedScene
+
+#just needs these information to id the whole room and the scene that the room was created on
---
+++ 01_Scripts/WorldInteractions/MapTransition.gd
@@ -0,0 +1,8 @@
+extends Node2D
+class_name MapTransition
+
+@export var room_a: MapRoom
+@export var exit_a_id: String
+
+@export var room_b: MapRoom
+@export var exit_b_id: String
