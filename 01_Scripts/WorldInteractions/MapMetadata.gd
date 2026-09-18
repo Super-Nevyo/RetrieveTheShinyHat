bc5574e191bf8cd31d6890992568b20816e3e16e
miss-nibiru <du.herbst@gmail.com>
2026-09-18 09:10:40 -0600

    Rooms know their exits IDs now!
    IDs also know what room they are connected to

---
+++ 01_Scripts/WorldInteractions/MapMetadata.gd
@@ -0,0 +1,17 @@
+#this is to make it so it knows how many exits are in my room
+
+class_name MapMetadata
+extends Node2D
+
+@export var transitions: Array[RoomTransition] = [] #all the room exists IDs will populate here
+
+func get_exit_data() -> Array[RoomTransitionData]:
+	var exits: Array[RoomTransitionData] = [] #makes an empty array every time it returns
+	
+	for transition in transitions:
+		if transition != null and transition.transition_data != null:
+			exits.append(transition.transition_data) #append = adds to the end of the array
+	
+	return exits
+
+#to add: The map shape of the mini map here when they are set
--- 01_Scripts/WorldInteractions/MapRoom.gd
+++ 01_Scripts/WorldInteractions/MapRoom.gd
@@ -1,7 +1,25 @@
+#this is a room in the world
+
 extends Node2D
 class_name MapRoom
 
 @export var room_id: String
 @export var room_scene: PackedScene
 
-#just needs these information to id the whole room and the scene that the room was created on
+func _ready() -> void:
+	print(room_id, " exit count: ", get_all_exits().size())
+	
+func get_all_exits() -> Array[RoomTransitionData]: #always start with an empty list
+	var exits: Array[RoomTransitionData] = []
+	
+	var new_room = room_scene.instantiate()
+	var metadata = new_room.get_node_or_null("MapMetadata") #make sure ALL rooms carry this node!!
+	print(room_id, "Metadata script found!")
+	
+	if metadata is MapMetadata :
+		print("I have some exits: ", metadata.transitions.size()) #size to return the elements in the array
+		exits = metadata.get_exit_data()
+	
+	new_room.free() # THIS IS TEMPORARY UNTIL CORPSE RECOVERY IS ADDED!
+	return exits
+	
