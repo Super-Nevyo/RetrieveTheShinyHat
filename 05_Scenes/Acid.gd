6862ff6c0168821b415f8a4a2cc5577dd9ecc6e8
miss-nibiru <du.herbst@gmail.com>
2026-09-21 09:16:00 -0600

    Platform floats!

--- 05_Scenes/Acid.gd
+++ 05_Scenes/Acid.gd
@@ -1,3 +1,17 @@
 extends Area2D
+class_name Acid
+
 
 #player has been added to a group - call "player" group to take damage
+#needs to move the marker so the platform detects movement - every physics frame?
+
+@onready var acid_surface: Marker2D = $AcidSurface
+@onready var starting_position: float = acid_surface.position.y
+var time: float = 0.0
+
+func move_marker_test() -> void:
+	acid_surface.position.y = starting_position + sin(time) * 5
+
+func _physics_process(delta: float) -> void:
+	time += delta
+	move_marker_test()
--- 05_Scenes/FloatingPlatform.gd
+++ 05_Scenes/FloatingPlatform.gd
@@ -1,19 +1,27 @@
 extends RigidBody2D
-
+class_name FloatingPlatform
 #Hookes Law: force is proportional to the distance of strech or compress. Elasticity.
 #F = -kx (Force = - object stiffness x distance stretch or squish
 #pulling the platform up makes the - pull it back down)
 
-@export var water_level: float = 500.0
+@export var acid_surface: Marker2D
 @export var stiffness: float = 1.0
-#@export var damping: float = 1.0
 
-func _physics_process(delta: float) -> void:
+#Damp force = -linear velocity+damping strenght
+@export var damping: float = 10.0
+
+func _physics_process(_delta: float) -> void:
 	
-	var depth: float = global_position.y - water_level
+	var depth: float = global_position.y - acid_surface.global_position.y
 	
 	if depth >= 0: #if its at water level
 		#hookes law applied here
 		var bouyancy_force: float = stiffness * depth
-		constant_force = Vector2 (0, -bouyancy_force) #built positional force
+		var damp_force = -linear_velocity.y * damping
+		
+		constant_force = Vector2 (0, -bouyancy_force + damp_force) #built positional force
 	
+	else:
+		constant_force = Vector2.ZERO
+		
+# to add: calculate the force 
